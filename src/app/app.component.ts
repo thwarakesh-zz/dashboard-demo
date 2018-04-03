12adb90183ac29031310a4c3a580187d6ec916dd
thuwarakeshm <thuwarakeshm@stax.com>
2018-04-03 18:54:01 +0530

    togglable sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent implements OnInit {
   loggedIn: boolean;
+  sideMenu_hidden = true;
   constructor(public auth: AuthService) {
   }
 
@@ -16,5 +17,16 @@ export class AppComponent implements OnInit {
     this.auth.getLoggedInStatus().subscribe(res => {
       this.loggedIn = res;
     });
+  }
+
+  toggleMenu() {
+    console.log('Sidemenu toggled!');
+    if (this.sideMenu_hidden) {
+      this.sideMenu_hidden = false;
+    } else {
+      this.sideMenu_hidden = true;
+    }
+
+    console.log(this.sideMenu_hidden);
   }
 }
