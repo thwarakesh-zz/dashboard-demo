e2c32e5a3cf7b134fcd6998c7b20f7965f6d79e9
thuwarakeshm <thuwarakeshm@stax.com>
2018-04-04 17:56:14 +0530

    dynamic sidemenu

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent implements OnInit {
+  sub_hidden = true;
+  deep_hidden = true;
+
+  menu_items = [
+    {
+      name: 'Overall',
+    }, {
+      name: 'Regional',
+      sub_items: [
+        {
+          name: 'India',
+          deep: [
+            {
+              name: 'Central India'
+            }, {
+              name: 'East India'
+            }, {
+              name: 'North India'
+            }, {
+              name: 'Northeast India'
+            }, {
+              name: 'South India'
+            }, {
+              name: 'Western India'
+            }
+          ]
+        }, {
+          name: 'Sri Lanka'
+        }, {
+          name: 'Bangaladesh',
+        }, {
+          name: 'Philipines'
+        }, {
+          name: 'Vietnam'
+        }
+      ]
+    }, {
+      name: 'Customer'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSub() {
+    this.sub_hidden = this.sub_hidden ? false : true;
+  }
+
+  toggleDeep() {
+    this.deep_hidden = this.deep_hidden ? false : true;
+    console.log('toggled Deep!');
   }
 
 }
